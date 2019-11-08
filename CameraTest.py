1659dd176d697c61e6110fc19e1311fe6babe7ca
Aye10032 <yeyu6_505@126.com>
2019-11-08 16:08:44 +0800

    test Camera resolution

--- CameraTest.py
+++ CameraTest.py
@@ -1,1 +1,21 @@
 import cv2
+import numpy as np
+
+capture1 = cv2.VideoCapture(2)
+capture2 = cv2.VideoCapture(3)
+capture1.set(3, 1280)
+capture1.set(4, 720)
+capture2.set(3, 1280)
+capture2.set(4, 720)
+
+while True:
+    ret1, frame1 = capture1.read()
+    ret2, frame2 = capture2.read()
+    frame1 = cv2.resize(frame1, (640, 360))
+    frame2 = cv2.resize(frame2, (640, 360))
+    frame = np.vstack((frame1, frame2))
+    height, width = frame1.shape[:2]
+    print(height, width)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
